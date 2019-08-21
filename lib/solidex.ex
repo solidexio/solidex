602b3ffd5ab6ddbcc8bc1c64d249a0214a09aa60
Georgy Shabunin <georgy.shabunin@gmail.com>
2019-08-21 23:42:32 +0200

    Initial commit
    Create a new elixir package (mix new solidex).

---
+++ lib/solidex.ex
@@ -0,0 +1,18 @@
+defmodule Solidex do
+  @moduledoc """
+  Documentation for Solidex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Solidex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/solidex_test.exs
@@ -0,0 +1,8 @@
+defmodule SolidexTest do
+  use ExUnit.Case
+  doctest Solidex
+
+  test "greets the world" do
+    assert Solidex.hello() == :world
+  end
+end
